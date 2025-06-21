bd395c49591e9067ab2f697b39342167596ee5eb
KimTaeGuk <devqabs@gmail.com>
2025-06-21 15:37:08 +0900

    update 백트래킹 베이스코드

--- algorithms/backTracking/BaseCode.java
+++ algorithms/backTracking/BaseCode.java
@@ -1,2 +1,35 @@
-package backTracking;public class BaseCode {
+package backTracking;
+
+import java.util.Scanner;
+
+public class BaseCode {
+	static int n, m;
+	static int[] arr = new int[10];
+	static boolean[] isUsed = new boolean[10];
+
+	public static void func(int k) { // 현재 k개까지 수를 택했음
+		if (k == m) { // m개를 모두 택했으면
+			for (int i = 0; i < m; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) { // 1부터 n까지의 수에 대해
+			if (!isUsed[i]) { // 아직 i가 사용되지 않았으면
+				arr[k] = i; // k번째 수를 i로 정함
+				isUsed[i] = true; // i를 사용되었다고 표시
+				func(k + 1); // 다음 수를 정하러 한 단계 더 들어감
+				isUsed[i] = false; // i를 이제 사용되지 않았다고 명시함
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		func(0);
+	}
 }
--- algorithms/backTracking/nextPermutation.java
+++ algorithms/backTracking/nextPermutation.java
@@ -1,2 +1,19 @@
-package backTracking;public class nextPermutation {
+package backTracking;
+
+public class nextPermutation {
+	public static boolean nextPermutation(int[] arr) {
+		int i = arr.length - 2;
+		while (i >= 0 && arr[i] >= arr[i + 1]) i--;
+		if (i < 0) return false;
+		int j = arr.length - 1;
+		while (arr[j] <= arr[i]) j--;
+		// swap arr[i] and arr[j]
+		int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+		// reverse from i+1 to end
+		for (int a = i + 1, b = arr.length - 1; a < b; a++, b--) {
+			temp = arr[a]; arr[a] = arr[b]; arr[b] = temp;
+		}
+		return true;
+	}
+
 }
